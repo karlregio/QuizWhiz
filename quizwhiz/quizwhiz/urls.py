d14732a035e7db640a1ad9287a8eda5d219db406
nash <karlregio619@gmail.com>
2026-04-23 22:50:37 +0800

    c1

--- quizwhiz/quizwhiz/urls.py
+++ quizwhiz/quizwhiz/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from quiz.views import index_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('quiz.urls')),
+    path('', index_view),
 ]
