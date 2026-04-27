e2c3d59b32f8c66f58fba5835e19fa731b4267bd
Mina Salapare <m.salapare11@gmail.com>
2026-04-27 19:10:30 +0800

    c14

--- quizwhiz/quiz/urls.py
+++ quizwhiz/quiz/urls.py
@@ -3,40 +3,59 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    # Pages
+    #Pages
     path('', index_view),
     path('quiz/', quiz_view),
     path('result/', result_view),
     path('leaderboard/', leaderboard_view),
     path('history/', history_view),
-    # ---------- Auth Routes ----------
+    #Auth Routes
     path('login/', login_view),
     path('register/', register_view),
     path('logout/', logout_view),
 
-    # ---------- PUBLIC API ----------
+    #PUBLIC API
     path('api/quizzes/<int:pk>/', quiz_detail),
     path('api/quizzes/<int:pk>/submit/', submit_quiz),
     path('api/leaderboard/', leaderboard_api),
     
-    # ---------- USER API ----------
+    #USER API
     path('api/my-results/', user_history),
     path('api/results/<int:pk>/', result_detail),
     
-    # ---------- PROFILE API ----------
+    #PROFILE 
     path('profile/', profile_view),
     path('api/profile/', profile_api),
 
-    # Categories & Topics
+    #Categories & Topics
     path('api/categories/', category_list),
     path('api/categories/<int:pk>/', category_detail),
     path('api/categories/<int:pk>/quizzes/', category_quizzes),
     path('category/', views.category_page, name='category_page'),
 
 
-    # ---------- ADMIN API ----------
-    path('api/quizzes/create/', create_quiz),              # POST
-    path('api/quizzes/<int:pk>/update/', update_quiz),     # PUT
-    path('api/quizzes/<int:pk>/delete/', delete_quiz),     # DELETE
-    
+    #ADMIN - Category CRUD
+    path('api/categories/create/', create_category),
+    path('api/categories/<int:pk>/update/', update_category),
+    path('api/categories/<int:pk>/delete/', delete_category),
+
+    #ADMIN - Quiz CRUD
+    path('api/quizzes/create/', create_quiz),              
+    path('api/quizzes/<int:pk>/update/', update_quiz),  
+    path('api/quizzes/<int:pk>/delete/', delete_quiz), 
+
+    #ADMIN - Question CRUD
+    path('api/questions/', question_list),
+    path('api/questions/create/', create_question),
+    path('api/questions/<int:pk>/', question_detail),
+    path('api/questions/<int:pk>/update/', update_question),
+    path('api/questions/<int:pk>/delete/', delete_question),
+
+    #ADMIN - Choice CRUD
+    path('api/choices/', choice_list),
+    path('api/choices/create/', create_choice),
+    path('api/choices/<int:pk>/', choice_detail),
+    path('api/choices/<int:pk>/update/', update_choice),
+    path('api/choices/<int:pk>/delete/', delete_choice),
+    path('api/quizzes/', quiz_list),
 ]
